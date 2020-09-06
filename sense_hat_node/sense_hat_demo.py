efedd68bee76480744391b5fe67b2e1afea36383
J-Pai <jesse.pai@gmail.com>
2020-09-06 07:03:09 +0000

    Updated demo code for sense-hat

--- sense_hat_node/sense_hat_demo.py
+++ sense_hat_node/sense_hat_demo.py
@@ -1,5 +1,13 @@
+#!/bin/env python3
+
 from sense_hat import SenseHat
 
 sense = SenseHat()
 
-sense.show_message('Hello World!')
+temperature = round(sense.get_temperature(), 1)
+print("Temperature: %s C" % temperature)
+
+humidity = round(sense.get_humidity(), 1)
+print("Humidity: %s %%rH" % humidity)
+
+sense.show_message("T:{t} H:{h}".format(t=temperature, h=humidity))
