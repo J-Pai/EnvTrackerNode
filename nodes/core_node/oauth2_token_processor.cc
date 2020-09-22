2a643922dd1f5a6fff24569ca274b918438b0e99
j-pai <jesse.pai@gmail.com>
2020-09-21 20:38:09 -0400

    Added printing of packet metadata to token parser.

--- nodes/core_node/oauth2_token_processor.cc
+++ nodes/core_node/oauth2_token_processor.cc
@@ -4,5 +4,10 @@ grpc::Status corenode::OAuth2TokenProcessor::Process(const InputMetadata &auth_m
     grpc::AuthContext *context, OutputMetadata *consumed_auth_metadata,
     OutputMetadata *response_metadata) {
   std::cout << "Using token processor..." << std::endl;
-  return grpc::Status::OK;
+  InputMetadata copy(auth_metadata);
+  std::multimap<grpc::string_ref, grpc::string_ref>::iterator itr;
+  for (itr = copy.begin(); itr != copy.end(); ++itr) {
+    std::cout << itr->first << "," << itr->second << std::endl;
+  }
+  return grpc::Status(grpc::StatusCode::UNAUTHENTICATED, "Missing token.");
 }
