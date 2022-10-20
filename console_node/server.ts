4871cc1016d265c9f267e956cb4ad5f2bb285c71
j-pai <jesse.pai@gmail.com>
2022-10-20 00:11:18 -0700

    Add boilerplate for frontend

--- console_node/server.ts
+++ console_node/server.ts
@@ -2,15 +2,7 @@ import express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get(["/", "/:name"], (req, res) => {
-  const greeting = "<h1>Hello From Console Node!</h1>";
-  const name = req.params["name"];
-  if (name) {
-    res.send(`${greeting} </br>and hello to ${name}`);
-  } else {
-    res.send(greeting);
-  }
-});
+app.use("/", express.static("frontend/dist"))
 
 app.listen(port, () => {
   console.log(`console_node app listening on port ${port}!`);
