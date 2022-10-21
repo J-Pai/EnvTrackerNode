2bc94f234661a7a58e5efe02350c73f9bfb7e698
j-pai <jesse.pai@gmail.com>
2022-10-20 23:26:28 -0700

    Add first API call

--- console_node/src/server/server.ts
+++ console_node/src/server/server.ts
@@ -1,12 +1,17 @@
 export {}
 
-const express = require("express");
+import express from "express";
+import type { Response } from "express";
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Serve statically generated frontend
 app.use("/", express.static("dist"));
 
+app.get("/v1/hello", (_, res: Response<string>) => {
+  res.send("Hello World");
+});
+
 app.listen(port, () => {
-  console.log(`console_node app listening on port ${port}!`);
+  console.log(`console_node app listening on http://localhost:${port}!`);
 });
