26de46f9b2299101523d1bd17c14882fe50cf342
j-pai <jesse.pai@gmail.com>
2022-10-22 12:31:23 -0700

    Demo edit on index.tsx page

--- console_node/pages/index.tsx
+++ console_node/pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://github.com/J-Pai/EnvTrackerNode">EnvTrackerNode - Console Node</a>
         </h1>
 
         <p className={styles.description}>
