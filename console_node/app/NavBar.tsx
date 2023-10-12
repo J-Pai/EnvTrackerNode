9025cf64e97fb26c47d1352751970f7b44367ce7
Jesse Pai <jessepai@google.com>
2023-10-12 13:30:24 -0700

    Add placeholder page icon.

--- console_node/app/NavBar.tsx
+++ console_node/app/NavBar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { Card, Flex, Heading } from "@radix-ui/themes";
+import { GearIcon } from "@radix-ui/react-icons";
+import { Box, Flex, Heading } from "@radix-ui/themes";
 
 import { useContext } from "react";
 
@@ -10,9 +11,16 @@ import ProfileDropdown from "./ProfileDropdown";
 export default function NavBar() {
   const { mobile } = useContext(ThemeContext);
 
+  const handleTitleBarClick = () => {
+    console.log("Redirect to home");
+  }
+
   return (
     <Flex gap="3" align="center" justify="start">
-      <Flex width="100%">
+      <Flex width="100%" gap="2" onClick={handleTitleBarClick}>
+        <Box width="5" height="5" mt="1" mx="1">
+          <GearIcon height="30" width="30" />
+        </Box>
         <Heading mt="1">Control Node</Heading>
       </Flex>
       <Flex gap="5" width="100%" align="center" justify="end">
