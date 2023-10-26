6ff923a143c2ac14a0c59ab4930539d24160be72
j-pai <jesse.pai@gmail.com>
2023-10-25 21:37:35 -0700

    Setup HTTPS for Rust API.

--- control_node/src/main.rs
+++ control_node/src/main.rs
@@ -1,5 +1,9 @@
-use actix_web::{post, web, App, HttpServer, Result, Responder};
-use serde::{Serialize, Deserialize};
+use std::{fs::File, io::BufReader};
+
+use actix_web::{post, web, App, HttpServer, Responder, Result};
+use rustls::{Certificate, PrivateKey, ServerConfig};
+use rustls_pemfile::{certs, pkcs8_private_keys};
+use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Debug)]
 struct EchoResp {
@@ -26,12 +30,38 @@ async fn echo(req_body: web::Json<EchoReq>) -> Result<impl Responder> {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    if std::env::var_os("CONSOLE_NODE_ROOT").is_none() {
-        std::env::set_var("CONSOLE_NODE_ROOT", "../console_node");
-    }
+    let config = load_rustls_config();
 
     HttpServer::new(|| App::new().service(echo))
-        .bind(("localhost", 8080))?
+        .bind_rustls_021(("localhost", 8443), config)?
         .run()
         .await
+}
+
+fn load_rustls_config() -> rustls::ServerConfig {
+    let config = ServerConfig::builder()
+        .with_safe_defaults()
+        .with_no_client_auth();
+
+    let cert_file = &mut BufReader::new(File::open("certificates/server.crt").unwrap());
+    let key_file = &mut BufReader::new(File::open("certificates/server.key").unwrap());
+
+    let cert_chain = certs(cert_file)
+        .unwrap()
+        .into_iter()
+        .map(Certificate)
+        .collect();
+
+    let mut keys: Vec<PrivateKey> = pkcs8_private_keys(key_file)
+        .unwrap()
+        .into_iter()
+        .map(PrivateKey)
+        .collect();
+
+    if keys.is_empty() {
+        eprintln!("Could not locate PKCS 8 private keys.");
+        std::process::exit(1);
+    }
+
+    config.with_single_cert(cert_chain, keys.remove(0)).unwrap()
 }
