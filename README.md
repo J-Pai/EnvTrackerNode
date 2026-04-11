8d6823f3332b1b6b23fa99eece87639e84952cc4
j-pai <jesse.pai@gmail.com>
2026-04-10 18:01:53 -0700

    Venv set up and example config file parsing.

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 # Environment Tracker Node
 
+### Example Config File
+
+```toml
+[settings]
+option1 = true
+option2 = false
+
+[kasa]
+
+[kasa.smart_plug]
+host = "123.456.789.123"
+username = "xxx"
+password = "xxx"
+```
+
 ### Development
 
 ```shell
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,76 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+use std::env;
+use std::fs;
+use std::io;
+use tokio::process::Command;
+use config::Config;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let home_dir = env::home_dir().expect("HOME dir not specified.");
+    let config_dir = format!("{}/.config/envtrackernode", home_dir.to_str().unwrap());
+    let config_file = format!("{}/config.toml", config_dir);
+    let settings = match Config::builder()
+        .add_source(config::File::with_name(&config_file))
+        .build() {
+        Ok(built_config) => {
+            println!("{:?}", built_config);
+            built_config
+        },
+        Err(e) => {
+            println!("Error obtaining config file: {:?}", e);
+            println!("Set up configuration? ([Y]es / [n]o)");
+            let mut response = String::new();
+            io::stdin().read_line(&mut response)?;
+            if response.to_uppercase().trim()  == "Y" {
+                println!("Creating {}", config_file);
+                fs::create_dir_all(config_dir)?;
+                fs::write(config_file, "")?;
+            } else if response.to_uppercase().trim() != "N" {
+                println!("Please specify either [Y]es or [N]o.")
+            }
+            return Ok(());
+        }
+    };
+
+    let _settings = match settings.try_deserialize::<HashMap<String, String>>() {
+        Ok(deserialized) => {
+            println!("{:?}", deserialized);
+        },
+        Err(e) => {
+            println!("Error parsing config file: {:?}", e);
+            return Ok(());
+        }
+    };
+
+    let mut venv_setup = Command::new("virtualenv")
+        .arg("venv")
+        .spawn()
+        .expect("Failed to setup virtualenv");
+
+    let status = venv_setup.wait().await?;
+
+    println!("==> virtualenv status: {status}");
+
+    let mut venv_requirements_setup = Command::new("venv/bin/pip")
+        .arg("install")
+        .arg("-r")
+        .arg("requirements.txt")
+        .spawn()
+        .expect("Failed to setup virtualenv");
+
+    let status = venv_requirements_setup.wait().await?;
+
+    println!("==> virtualenv requirements setup status: {status}");
+
+    let mut venv_requirements_setup = Command::new("venv/bin/kasa")
+        .arg("install")
+        .arg("-r")
+        .arg("requirements.txt")
+        .spawn()
+        .expect("Failed to setup virtualenv");
+
+    let status = venv_requirements_setup.wait().await?;
+
+    Ok(())
 }
