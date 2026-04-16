890fa5d754b32caa059b300dc5fe286007d892a6
Jesse Pai <jesse.pai@gmail.com>
2026-04-15 23:36:36 -0700

    Add `release` option to launch.sh

--- launch.sh
+++ launch.sh
@@ -2,6 +2,12 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+RELEASE_FLAG=""
+
+if [[ "$1" == "release" ]]; then
+	RELEASE_FLAG="--release"
+fi
+
 cd $SCRIPT_DIR
-trunk build
-cargo run
+trunk build $RELEASE_FLAG
+cargo run $RELEASE_FLAG
