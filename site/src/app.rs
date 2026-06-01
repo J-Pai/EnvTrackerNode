fd096e872c65784787645d96aee66c5363e0a8d6
Jesse Pai <jesse.pai@gmail.com>
2026-05-31 21:02:49 -0700

    Added frame time + FPS counter.

--- site/src/app.rs
+++ site/src/app.rs
@@ -4,40 +4,54 @@ use egui::Widget;
 use egui::util::History;
 use egui_tiles::SimplificationOptions;
 
-/// Persistent state tracking.
-/// We derive Deserialize/Serialize so we can persist app state on shutdown.
-#[derive(Default, serde::Deserialize, serde::Serialize)]
-pub struct State {
-    control_panel: bool,
-    tile_tree: Option<egui_tiles::Tree<Pane>>,
+pub struct FrameHistory {
+    frame_times: History<f32>,
 }
 
-pub struct EnvApp {
-    state: State,
-    frame_time: History<f32>,
-}
-
-impl EnvApp {
-    /// Called once before the first frame.
-    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+impl Default for FrameHistory {
+    fn default() -> Self {
         let max_age: f32 = 1.0;
         let max_len = (max_age * 300.0).round() as usize;
-        let frame_time = History::new(0..max_len, max_age);
-
-        if let Some(storage) = cc.storage {
-            Self {
-                state: eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default(),
-                frame_time,
-            }
-        } else {
-            Self {
-                state: Default::default(),
-                frame_time,
-            }
-        }
+        let frame_times = History::new(0..max_len, max_age);
+        Self { frame_times }
     }
 }
 
+impl FrameHistory {
+    // Called first
+    pub fn on_new_frame(&mut self, now: f64, previous_frame_time: Option<f32>) {
+        let previous_frame_time = previous_frame_time.unwrap_or_default();
+        if let Some(latest) = self.frame_times.latest_mut() {
+            *latest = previous_frame_time; // rewrite history now that we know
+        }
+        self.frame_times.add(now, previous_frame_time); // projected
+    }
+
+    pub fn mean_frame_time(&self) -> f32 {
+        self.frame_times.average().unwrap_or_default()
+    }
+
+    pub fn fps(&self) -> f32 {
+        1.0 / self.frame_times.mean_time_interval().unwrap_or_default()
+    }
+
+    pub fn ui(&self, ui: &mut egui::Ui) {
+        ui.label(format!(
+            "Mean CPU usage: {:.2} ms / frame",
+            1e3 * self.mean_frame_time()
+        ))
+        .on_hover_text(
+            "Includes all app logic, egui layout, tessellation, and rendering.\n\
+            Does not include waiting for vsync.",
+        );
+        ui.label(format!("FPS: {:.2}", self.fps())).on_hover_text(
+            "Includes all app logic, egui layout, tessellation, and rendering.\n\
+            Does not include waiting for vsync.",
+        );
+    }
+}
+
+/// Tile/Pane rendering.
 #[derive(Default, serde::Deserialize, serde::Serialize)]
 struct Pane {
     nr: i32,
@@ -90,6 +104,37 @@ impl egui_tiles::Behavior<Pane> for TreeBehavior {
     }
 }
 
+/// Persistent state tracking.
+/// We derive Deserialize/Serialize so we can persist app state on shutdown.
+#[derive(Default, serde::Deserialize, serde::Serialize)]
+pub struct State {
+    control_panel: bool,
+    continuous: bool,
+    tile_tree: Option<egui_tiles::Tree<Pane>>,
+}
+
+pub struct EnvApp {
+    state: State,
+    frame_history: FrameHistory,
+}
+
+impl EnvApp {
+    /// Called once before the first frame.
+    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        if let Some(storage) = cc.storage {
+            Self {
+                state: eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default(),
+                frame_history: Default::default(),
+            }
+        } else {
+            Self {
+                state: Default::default(),
+                frame_history: Default::default(),
+            }
+        }
+    }
+}
+
 impl EnvApp {
     fn create_tree(&mut self) {
         if self.state.tile_tree.is_some() {
@@ -122,6 +167,14 @@ impl eframe::App for EnvApp {
     /// Called by the framework to save state before shutdown.
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
         eframe::set_value(storage, eframe::APP_KEY, &self.state);
+    }
+
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        self.frame_history
+            .on_new_frame(ctx.input(|i| i.time), frame.info().cpu_usage);
+        if self.state.continuous {
+            ctx.request_repaint();
+        }
     }
 
     fn clear_color(&self, visuals: &egui::Visuals) -> [f32; 4] {
@@ -163,11 +216,12 @@ impl eframe::App for EnvApp {
                             ui.heading("💻 Control Panel");
                         });
                         ui.separator();
-                        if ui.button("Reset").clicked() {
+                        if ui.button("Reset Tiles").clicked() {
                             self.reset_tree();
                         };
                         ui.separator();
-                        ui.label("Mean CPU usage: 00.00 ms / frame");
+                        self.frame_history.ui(ui);
+                        ui.checkbox(&mut self.state.continuous, "Run Mode - Continuous");
                         ui.separator();
                     });
                     ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
