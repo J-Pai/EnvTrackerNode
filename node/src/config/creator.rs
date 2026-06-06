09623b2c0ff2c9e0deb3898eb94a11c556fb6e38
Jesse Pai <jesse.pai@gmail.com>
2026-06-06 16:27:02 -0700

    Initial configuration UI for target nodes.

--- node/src/config/creator.rs
+++ node/src/config/creator.rs
@@ -1,7 +1,9 @@
 //! TUI for creating new server configs.
 
+use std::collections::HashMap;
 use std::fs;
 use std::path::PathBuf;
+use std::rc::Rc;
 
 use appcui::prelude::*;
 
@@ -18,18 +20,20 @@ use crate::error::NodeError;
 
 pub(super) struct Creator {
     app: Option<App>,
-    config: ServerConfig,
+    config: Rc<ServerConfig>,
 }
 
 impl Creator {
     pub(super) fn new(config: ServerConfig) -> Result<Self, appcui::system::Error> {
         let mut app = App::new().single_window().build()?;
-        let window = CreatorWindow::new();
+        let config = Rc::new(config);
+        let window = CreatorWindow::new(config.clone());
         app.add_window(window);
-        Ok(Self {
+        let app = Self {
             app: Some(app),
             config,
-        })
+        };
+        Ok(app)
     }
 
     /// Launches the creator TUI session.
@@ -43,57 +47,152 @@ impl Creator {
     }
 
     pub(super) fn write(self, path: PathBuf) -> ServerConfig {
-        let config_text =
-            toml::to_string_pretty(&self.config).expect("Could not convert config to toml.");
+        let config_text = toml::to_string_pretty(self.config.as_ref())
+            .expect("Could not convert config to toml.");
         fs::write(&path, config_text).expect("Failed to write config file.");
 
-        self.config
+        self.config.as_ref().clone()
     }
 }
 
 #[Window(events = ButtonEvents)]
-pub(super) struct CreatorWindow {}
+struct CreatorWindow {
+    config: Rc<ServerConfig>,
+    api_server: Option<ApiServerUi>,
+    frontend_server: Option<FrontendServerUi>,
+    node_server: Option<NodeUi>,
+}
 
 impl CreatorWindow {
-    fn new() -> Self {
-        let mut window = Self {
-            base: window!("'ServerConfig',dock:fill"),
-        };
-
+    fn new(config: Rc<ServerConfig>) -> Self {
         let mut tabs =
             tab!("d:f, tabs: ['API Server', 'Frontend Server', 'Node'], tw: 32, type: OnTop");
-        window = window.api_server(0, &mut tabs);
+
+        let mut window = Self {
+            config,
+            base: window!("'ServerConfig',dock:fill"),
+            api_server: Some(ApiServerUi::new(&mut tabs, 0)),
+            frontend_server: Some(FrontendServerUi::new(&mut tabs, 1)),
+            node_server: Some(NodeUi::new(&mut tabs, 2)),
+        };
+
         window.add(tabs);
 
         window
-    }
-
-    fn api_server(self, tab_index: u32, tab: &mut Tab) -> Self {
-        let mut form_panel = Panel::new("", layout!("x:0, y:0, w: 50%, h: 100%"));
-
-        let enable = checkbox!("'Enable Service', x:1, y:0, w:32");
-        form_panel.add(enable);
-        let label = label!("'Database Path:', x:1, y:2, w: 14");
-        form_panel.add(label);
-        let db = textfield!("caption='sqlite.db', x:16, y:2, w:32");
-        form_panel.add(db);
-
-        tab.add(tab_index, form_panel);
-
-        let mut node_panel = Panel::new("", layout!("x:50%, y:0, w: 50%, h: 100%"));
-
-        let add_node = button!("'Add Node', x:1, y:0, w:16");
-        node_panel.add(add_node);
-
-        tab.add(tab_index, node_panel);
-
-        self
     }
 }
 
 impl ButtonEvents for CreatorWindow {
     fn on_pressed(&mut self, handle: Handle<Button>) -> EventProcessStatus {
-        tracing::debug!("Pressed! {:?}", handle);
+        if handle == self.api_server.as_ref().unwrap().add_node_button {
+            let mut api_server = self.api_server.take().unwrap();
+            api_server.add_node(self);
+            self.api_server.replace(api_server);
+            return EventProcessStatus::Processed;
+        }
+
+        if handle == self.api_server.as_ref().unwrap().save_button {
+            let mut api_server = self.api_server.take().unwrap();
+            self.api_server.replace(api_server);
+            return EventProcessStatus::Processed;
+        }
+
         EventProcessStatus::Ignored
+    }
+}
+
+struct ApiServerUi {
+    enable: Handle<CheckBox>,
+    save_button: Handle<Button>,
+    db_field: Handle<TextField>,
+    node_panel: Handle<Panel>,
+    add_node_button: Handle<Button>,
+    node_configs: Vec<(Handle<Button>, Handle<Panel>)>,
+}
+
+impl ApiServerUi {
+    fn new(tabs: &mut Tab, index: u32) -> Self {
+        let mut form_panel = Panel::new("", layout!("x:0, y:0, w: 50%, h: 100%"));
+
+        let enable = checkbox!("'Enable Service', x:1, y:0, w:32");
+        let enable = form_panel.add(enable);
+        let save = button!("'Save', x:32, y:0, w:32");
+        let save = form_panel.add(save);
+        let label = label!("'Database Path:', x:1, y:2, w: 14");
+        form_panel.add(label);
+        let db = form_panel.add(textfield!("caption='sqlite.db', x:32, y:2, w:32"));
+        tabs.add(index, form_panel);
+
+        let mut node_panel = Panel::new("", layout!("x:50%, y:0, w: 50%, h: 100%"));
+        let add_node = node_panel.add(button!("'Add Node', x:1, y:0, w:16"));
+        let node_panel = tabs.add(index, node_panel);
+
+        Self {
+            enable,
+            save_button: save,
+            db_field: db,
+            node_panel: node_panel,
+            add_node_button: add_node,
+            node_configs: vec![],
+        }
+    }
+
+    fn add_node(&mut self, window: &mut CreatorWindow) {
+        const NODE_HEIGHT: u16 = 7;
+        const NODE_START_Y: u16 = 2;
+        let index = self.node_configs.len() as u16;
+        let node_panel = window.control_mut(self.node_panel).unwrap();
+        let mut panel = Panel::new(
+            format!("Node {}", index).as_str(),
+            layout!("x: 0, y: 2, w: 100%, h: 5"),
+        );
+        panel.set_position(0, (NODE_HEIGHT * index + NODE_START_Y) as i32);
+        panel.set_size(node_panel.size().width as u16 - 2, NODE_HEIGHT);
+        panel.add(label!("'Node Name:', x:0, y:0, w: 32"));
+        panel.add(textfield!("caption='node_name', x:32, y:0, w: 32"));
+        panel.add(label!("'IP:', x:0, y:1, w: 32"));
+        panel.add(textfield!("caption='0.0.0.0:3000', x:32, y:1, w: 32"));
+        panel.add(label!("'Polling Schedule:', x:0, y:2, w: 32"));
+        panel.add(textfield!("caption='0 * * * * *', x:32, y:2, w: 32"));
+        let button = button!("Remove, x:0, y:3, w:10");
+        self.node_configs
+            .push((panel.add(button), node_panel.add(panel)));
+        window.request_update();
+    }
+}
+
+struct FrontendServerUi {
+    enable: Handle<CheckBox>,
+}
+
+impl FrontendServerUi {
+    fn new(tabs: &mut Tab, index: u32) -> Self {
+        let mut form_panel = Panel::new("", layout!("x:0, y:0, w: 50%, h: 100%"));
+
+        let enable = checkbox!("'Enable Service', x:1, y:0, w:32");
+        let enable = form_panel.add(enable);
+        let save = button!("'Save', x:32, y:0, w:32");
+        let save = form_panel.add(save);
+        tabs.add(index, form_panel);
+
+        Self { enable }
+    }
+}
+
+struct NodeUi {
+    enable: Handle<CheckBox>,
+}
+
+impl NodeUi {
+    fn new(tabs: &mut Tab, index: u32) -> Self {
+        let mut form_panel = Panel::new("", layout!("x:0, y:0, w: 50%, h: 100%"));
+
+        let enable = checkbox!("'Enable Service', x:1, y:0, w:32");
+        let enable = form_panel.add(enable);
+        let save = button!("'Save', x:32, y:0, w:32");
+        let save = form_panel.add(save);
+        tabs.add(index, form_panel);
+
+        Self { enable }
     }
 }
