7e417fdd42c6548446934aa94bba5068b19240c8
Jesse Pai <jesse.pai@gmail.com>
2026-06-20 13:17:30 -0700

    Bumping rust version to 1.96.

--- site/src/main.rs
+++ site/src/main.rs
@@ -1,5 +1,4 @@
 #![warn(clippy::all, rust_2018_idioms)]
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
 // When compiling natively:
 fn main() {
