35435f042e897a4959fa9ffe368da2144f2377ec
Jesse Pai <jesse.pai@gmail.com>
2026-06-21 10:28:56 -0700

    Some more logic for api and refactor poller to be flatter.

--- node/src/services/poller.rs
+++ node/src/services/poller.rs
@@ -9,7 +9,9 @@ use tokio_cron_scheduler::Job;
 use tokio_cron_scheduler::JobScheduler;
 
 use crate::config::ApiServerConfig;
+use crate::config::KasaDeviceConfig;
 use crate::config::NodeClass;
+use crate::config::PollingConfig;
 use crate::services::db::Db;
 use crate::services::kasa::KasaChildInfo;
 
@@ -27,86 +29,100 @@ impl Poller {
         self,
         config: &ApiServerConfig,
     ) -> Result<Self, Box<dyn std::error::Error>> {
+        let mut poller = self;
         let nodes = config.get_nodes();
 
         for node in nodes {
             let node = node.clone();
-            let scheduler = self.scheduler.read().await;
-            let node_client = ClientBuilder::new(Client::new()).build();
-            let db = self.db.as_ref().unwrap().clone();
 
             if let NodeClass::KasaDevice(topic, device_config, polling) = node {
-                let route = polling.clone().get_api().unwrap_or_default();
+                let route = match polling.clone().get_api() {
+                    Some(endpoint) => endpoint,
+                    None => String::new(),
+                };
 
-                let job = Job::new_async(polling.get_schedule(), move |_uuid, _l| {
-                    Box::pin({
-                        let topic = topic.clone();
-                        let device_config = device_config.clone();
-                        let route = route.clone();
-                        let mut db = db.clone();
-                        let node_client = node_client.clone();
-                        async move {
-                            if let Err(e) = db.create_connection().await {
-                                tracing::warn!("Unable to create connection: {:#?}", e);
-                                return;
-                            };
-
-                            let mut sample_count = 0;
-                            let url = format!("http://{}{}", device_config.get_ip(), route);
-
-                            loop {
-                                sample_count += 1;
-
-                                match node_client.get(&url).send().await {
-                                    Ok(data) => {
-                                        let json = data.text().await.unwrap_or("[]".to_string());
-
-                                        match serde_json::from_str::<Vec<Vec<KasaChildInfo>>>(&json)
-                                        {
-                                            Ok(data) => {
-                                                if data.is_empty() {
-                                                    break;
-                                                }
-                                                if let Err(e) =
-                                                    db.push_kasa_data(&topic, &data).await
-                                                {
-                                                    tracing::warn!(
-                                                        "Failed to write data: {:#?}",
-                                                        e
-                                                    );
-                                                }
-                                            }
-                                            Err(e) => {
-                                                tracing::warn!("Error parsing data {:#?}", e);
-                                            }
-                                        }
-                                    }
-                                    Err(e) => {
-                                        tracing::warn!(
-                                            "Issue with: {}{} - {:#?}",
-                                            device_config.get_ip(),
-                                            route,
-                                            e
-                                        );
-                                        break;
-                                    }
-                                }
-                            }
-
-                            tracing::debug!(
-                                "Requested {}x {}{}",
-                                sample_count,
-                                device_config.get_ip(),
-                                route
-                            );
-                        }
-                    })
-                })?;
-
-                scheduler.add(job).await?;
+                poller = poller
+                    .add_kasa_job(route, topic, device_config, polling)
+                    .await?;
             }
         }
 
+        Ok(poller)
+    }
+
+    async fn add_kasa_job(
+        self,
+        route: String,
+        topic: String,
+        device_config: KasaDeviceConfig,
+        polling: PollingConfig,
+    ) -> Result<Self, Box<dyn std::error::Error>> {
+        let scheduler = self.scheduler.read().await;
+        let node_client = ClientBuilder::new(Client::new()).build();
+        let db = self.db.as_ref().unwrap().clone();
+
+        let job = Job::new_async(polling.get_schedule(), move |_uuid, _l| {
+            Box::pin({
+                let topic = topic.clone();
+                let device_config = device_config.clone();
+                let route = route.clone();
+                let mut db = db.clone();
+                let node_client = node_client.clone();
+                async move {
+                    if let Err(e) = db.create_connection().await {
+                        tracing::warn!("Unable to create connection: {:#?}", e);
+                        return;
+                    };
+
+                    let mut sample_count = 0;
+                    let url = format!("http://{}{}", device_config.get_ip(), route);
+
+                    loop {
+                        sample_count += 1;
+
+                        match node_client.get(&url).send().await {
+                            Ok(data) => {
+                                let json = data.text().await.unwrap_or("[]".to_string());
+
+                                match serde_json::from_str::<Vec<Vec<KasaChildInfo>>>(&json) {
+                                    Ok(data) => {
+                                        if data.is_empty() {
+                                            break;
+                                        }
+                                        if let Err(e) = db.push_kasa_data(&topic, &data).await {
+                                            tracing::warn!("Failed to write data: {:#?}", e);
+                                        }
+                                    }
+                                    Err(e) => {
+                                        tracing::warn!("Error parsing data {:#?}", e);
+                                    }
+                                }
+                            }
+                            Err(e) => {
+                                tracing::warn!(
+                                    "Issue with: {}{} - {:#?}",
+                                    device_config.get_ip(),
+                                    route,
+                                    e
+                                );
+                                break;
+                            }
+                        }
+                    }
+
+                    tracing::debug!(
+                        "Requested {}x {}{}",
+                        sample_count,
+                        device_config.get_ip(),
+                        route
+                    );
+                }
+            })
+        })?;
+
+        scheduler.add(job).await?;
+
+        drop(scheduler);
         Ok(self)
     }
 
--- node/src/services/web/api.rs
+++ node/src/services/web/api.rs
@@ -1,13 +1,34 @@
 //! Logic for handling API calls from the frontend (or other clients).
 
 use crate::config::ApiServerConfig;
+use crate::config::KasaDeviceConfig;
+use crate::config::NodeClass;
 
 use super::Web;
 
 impl Web {
     pub(crate) async fn setup_api_route(
         self,
-        _config: &ApiServerConfig,
+        config: &ApiServerConfig,
+    ) -> Result<Self, Box<dyn std::error::Error>> {
+        let mut web = self;
+        for node in config.get_nodes() {
+            web = match node {
+                NodeClass::KasaDevice(topic, config, _) => {
+                    web.setup_kasa_api_route(topic, config).await?
+                }
+
+                NodeClass::Unknown => continue,
+            };
+        }
+
+        Ok(web)
+    }
+
+    async fn setup_kasa_api_route(
+        self,
+        topic: &String,
+        config: &KasaDeviceConfig,
     ) -> Result<Self, Box<dyn std::error::Error>> {
         Ok(self)
     }
