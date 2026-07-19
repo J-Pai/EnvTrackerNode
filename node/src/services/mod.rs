fb6ab79ecc46dae09807aa1fa5447b06a58a8f04
Jesse Pai <jesse.pai@gmail.com>
2026-07-18 22:59:19 -0700

    Add verified IdToken example.
    - Re-org a bit for Auth specific handling in DB.
    - New module for auth since we need to extract certs from
      `https://www.googleapis.com/oauth2/v1/certs`.

--- node/src/services/mod.rs
+++ node/src/services/mod.rs
@@ -1,3 +1,4 @@
+pub(crate) mod auth;
 pub(crate) mod db;
 pub(crate) mod kasa;
 pub(crate) mod poller;
--- node/src/services/web/auth.rs
+++ node/src/services/web/auth.rs
@@ -1,5 +1,6 @@
 //! Authentication handlers.
 
+use std::collections::HashMap;
 use std::fs;
 use std::path::PathBuf;
 use std::sync::Arc;
@@ -12,7 +13,13 @@ use axum_oidc_client::auth_builder::OAuthConfigurationBuilder;
 use axum_oidc_client::auth_cache::AuthCache;
 use axum_oidc_client::auth_session::AuthSession;
 use axum_oidc_client::extractors::OptionalAuthSession;
+use axum_oidc_client::jwt::Algorithm;
+use axum_oidc_client::jwt::DecodingKey;
+use axum_oidc_client::jwt::Validation;
+use axum_oidc_client::jwt::decode_jwt;
+use axum_oidc_client::jwt::decode_jwt_unverified;
 use axum_oidc_client::logout::handle_default_logout::DefaultLogoutHandler;
+use tokio::sync::RwLock;
 use tower_sessions::cookie::Key;
 
 use crate::config::FrontendServerConfig;
@@ -45,6 +52,8 @@ impl Web {
         Ok(json.web)
     }
 
+    fn request_google_oauth2_certs(cers: RwLock<HashMap<String, DecodingKey>>) {}
+
     pub(crate) async fn setup_auth(
         mut self,
         oauth2_config: &OAuth2Config,
@@ -75,21 +84,15 @@ impl Web {
             .with_scopes(vec!["openid", "email", "profile"])
             .with_code_challenge_method(CodeChallengeMethod::S256)
             .with_post_logout_redirect_uri(&format!("{base}/google_home/login"))
-            .with_session_max_age(30)
-            .with_token_max_age(300)
+            .with_session_max_age(60 * 25 * 365)
             .with_base_path("/auth");
-
         config.private_cookie_key = Some(Key::from(&key[..64]));
-
         let config = config.build()?;
-
-        let cache: Arc<dyn AuthCache + Send + Sync> = Arc::new(self.db.as_ref().unwrap().clone());
 
         let logout_handler = Arc::new(DefaultLogoutHandler);
         let base_redirect = oauth2_config.get_redirect_uri_base();
 
         let google_home_link = move |_session: AuthSession| async move {};
-
         let google_home_login = move |OptionalAuthSession(session): OptionalAuthSession| async move {
             match session {
                 Some(session) => {
@@ -97,6 +100,24 @@ impl Web {
                         .expires
                         .map(|e| e.to_string())
                         .unwrap_or_else(|| "(no expiry)".to_string());
+                    tracing::info!("token {:#?}", session.id_token);
+
+                    let token_data = decode_jwt_unverified(&session.id_token)
+                        .map_err(|e| tracing::error!("JWT Error: {e}"));
+                    tracing::info!("JWT data unverified {:#?}", token_data);
+
+                    let key = DecodingKey::from_rsa_pem(
+                        "-----BEGIN CERTIFICATE-----\nMIIDJzCCAg+gAwIBAgIJAKInBBbLHDHYMA0GCSqGSIb3DQEBBQUAMDYxNDAyBgNV\nBAMMK2ZlZGVyYXRlZC1zaWdub24uc3lzdGVtLmdzZXJ2aWNlYWNjb3VudC5jb20w\nHhcNMjYwNzEwMTc1OTE4WhcNMjYwNzI3MDYxNDE4WjA2MTQwMgYDVQQDDCtmZWRl\ncmF0ZWQtc2lnbm9uLnN5c3RlbS5nc2VydmljZWFjY291bnQuY29tMIIBIjANBgkq\nhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsjGSud3Gx+92yeucu7BIAhvzkybkL21e\nOCejL9t2JMpqy6cMThhS2Dtr+ByKdjtoD0GLP8LCT2yJfJH5YAbHVbvBU88eUsbG\nd7ZlaYqicTD5Pc6B/BO8LjSr3YH1kVoOcn8Lct31+EhloVAIxBLROsS2489N3bwW\nOLaOhnYCLvMWqVFqV5TJPMbIBzADXeJmAyF/K2uP5P7KDWYlGz2V6AH7aS6n3/K0\nvdb8SVeqCu8N0M5SifpSUMQidVp5Ku+wd0Yu6P9mZcAzS9GuzePJMNsbKjDkITlc\n1k+KZMO2RH23zAbMCNqVABQRFLCQhulYEAbd+sYbulsrHaw4MYo3YwIDAQABozgw\nNjAMBgNVHRMBAf8EAjAAMA4GA1UdDwEB/wQEAwIHgDAWBgNVHSUBAf8EDDAKBggr\nBgEFBQcDAjANBgkqhkiG9w0BAQUFAAOCAQEAEww8cWooeHcykMf6g5WEf/CXPdlO\nkqrebFA9RLjf3rLB/ausHSgYkvHg8wq3rWICDPA3VmR6YjgLpQ+EmO8ShyFn75Cp\nPq3SwUJt3AEuNUiAYgF6xp2JUkMAjCpFfBYcyWJIia2+WCsKYB83dMmhOxAsKdn2\n0GGn4uGnrltA+3QYtTW7cy4IplPVT4XrCIVKFj9rnqJujpr1zHTN9eqML8ovtiyO\nKniDV7SHS5I8tfZ39EKOEZzOCrhyy/2ZDgqqMwjl8YmTCiHJk+AGDmDPGcZZGdH3\nRanMPhXXF59N7hXusIHuX2BZ/Ypg4VIp2VCEheZboLdwfufybdn0mZ52zQ==\n-----END CERTIFICATE-----\n".as_bytes(),
+                    ).unwrap();
+                    let mut validation = Validation::new(Algorithm::RS256);
+                    validation.set_audience(&[
+                        "42441590702-te8vdhfdd8s3ft960ct6ksle7hp25jtj.apps.googleusercontent.com",
+                    ]);
+
+                    let token_data = decode_jwt(&session.id_token, &key, &validation)
+                        .map_err(|e| tracing::error!("JWT Error: {e}"));
+                    tracing::info!("JWT data verified {:#?}", token_data);
+
                     Html(format!(
                         r#"
                             Hello World for Google Home: expires {expires}
@@ -113,11 +134,12 @@ impl Web {
                 )),
             }
         };
-
         let google_home_fulfillment = move |_session: AuthSession| async move {
             tracing::debug!("Handling fulfillment");
             "{}"
         };
+
+        let cache: Arc<dyn AuthCache + Send + Sync> = Arc::new(self.db.as_ref().unwrap().clone());
 
         router = router
             .route("/google_home/link", routing::get(google_home_link))
