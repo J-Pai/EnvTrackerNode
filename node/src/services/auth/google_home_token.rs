3ed2031c464220554bac1a08e4e5976aae776a16
Jesse Pai <jesse.pai@gmail.com>
2026-07-21 22:44:21 -0700

    Resolved token request issue.
    Able to obtain token from Google OAuth2 now.

--- node/src/services/auth/google_home_token.rs
+++ node/src/services/auth/google_home_token.rs
@@ -85,7 +85,7 @@ impl Auth {
             }
 
             let form = HashMap::from([
-                ("cliend_id", google_home_client_json.client_id),
+                ("client_id", google_home_client_json.client_id),
                 ("client_secret", google_home_client_json.client_secret),
                 ("grant_type", "authorization_code".to_string()),
                 ("code", code.clone()),
@@ -109,16 +109,24 @@ impl Auth {
             };
 
             tracing::info!("OAuth2 Response: {data:#?}");
+            let status = data.status();
 
-            let Ok(data) = data
-                .error_for_status()
-                .map_err(|e| tracing::error!("OAuth2 Response Error {e}"))
-            else {
+            let Ok(body) = &data.text().await else {
+                tracing::error!("Unable to convert body.");
                 return (StatusCode::BAD_REQUEST, Json::from(invalid_response_json))
                     .into_response();
             };
 
-            tracing::info!("OAuth2 Response Body: {data:#?}");
+            if status != StatusCode::OK {
+                let body = Json::from(body);
+                tracing::error!("OAuth2 Request Error: {:#?}", body);
+                return (StatusCode::BAD_REQUEST, Json::from(invalid_response_json))
+                    .into_response();
+            }
+
+            let body = Json::from(body);
+
+            tracing::info!("OAuth2 Response Body: {body:#?}");
         } else if &form.grant_type == "refresh_token"
             && let Some(refresh_token) = &form.code
             && let Ok(Some(refresh_state)) = db.get_auth_session(refresh_token).await
